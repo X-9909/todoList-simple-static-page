018bd45162d80f6ffa357a183a05cc12451a9173
xiekunlou <10025483+xie-kunlou@user.noreply.gitee.com>
2024-12-14 14:44:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoMain from "./components/TodoMain.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+</script>
+
+<template>
+  <div class="todoapp">
+    <TodoHeader />
+    <TodoMain />
+    <TodoFooter />
+  </div>
+</template>
+
+<style scoped></style>
